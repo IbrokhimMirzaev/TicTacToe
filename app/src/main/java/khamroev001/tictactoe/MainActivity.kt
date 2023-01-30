d65689f9cfb2659cdb8fcbd0529b69446df568cc
IbrokhimMirzaev <mirzaevibrokhim@gmail.com>
2023-01-30 23:07:05 +0500

    new changes

--- app/src/main/java/khamroev001/tictactoe/MainActivity.kt
+++ app/src/main/java/khamroev001/tictactoe/MainActivity.kt
@@ -2,10 +2,16 @@ package khamroev001.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onClick(p0: View?) {
     }
 }
