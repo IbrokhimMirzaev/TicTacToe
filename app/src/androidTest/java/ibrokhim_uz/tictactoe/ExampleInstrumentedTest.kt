ed29a9a580bb20370d18c7d878b2bdea1584172e
IbrokhimMirzaev <mirzaevibrokhim@gmail.com>
2023-01-31 20:34:16 +0500

    new changes

--- app/src/androidTest/java/ibrokhim_uz/tictactoe/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ibrokhim_uz/tictactoe/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("khamroev001.tictactoe", appContext.packageName)
+        assertEquals("ibrokhim_uz.tictactoe", appContext.packageName)
     }
 }
