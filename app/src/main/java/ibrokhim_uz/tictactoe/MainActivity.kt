4fa8481c776e5c888974e9f121e456d2f7abbd06
IbrokhimMirzaev <mirzaevibrokhim@gmail.com>
2023-01-31 21:21:48 +0500

    new changes

--- app/src/main/java/ibrokhim_uz/tictactoe/MainActivity.kt
+++ app/src/main/java/ibrokhim_uz/tictactoe/MainActivity.kt
@@ -7,92 +7,180 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    var active: Boolean = true
-
     var matrix = Array(3) { IntArray(3) { -1 } }
-
+    var active = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        a1.setOnClickListener(this)
-        a2.setOnClickListener(this)
-        a3.setOnClickListener(this)
-        b1.setOnClickListener(this)
-        b2.setOnClickListener(this)
-        b3.setOnClickListener(this)
-        c1.setOnClickListener(this)
-        c2.setOnClickListener(this)
-        c3.setOnClickListener(this)
-    }
+        active_player.text = "Player X"
 
-    override fun onClick(v: View?) {
-        val img = findViewById<ImageButton>(v!!.id)
-        val tag = img.tag.toString().toInt() // 0,1,2,3,4,5,6,7,8
-        val col: Int = tag / 3
-        val row: Int = tag % 3
-        if (active) {
-            img.setImageResource(R.drawable.x_sign)
-            active = false
-            matrix[col][row] = 1
-            isWinner(1)
-        } else {
-            img.setImageResource(R.drawable.o_sign)
-            active = true
-            matrix[col][row] = 0
-            isWinner(0)
+        img0.setOnClickListener(this)
+        img1.setOnClickListener(this)
+        img2.setOnClickListener(this)
+        img3.setOnClickListener(this)
+        img4.setOnClickListener(this)
+        img5.setOnClickListener(this)
+        img6.setOnClickListener(this)
+        img7.setOnClickListener(this)
+        img8.setOnClickListener(this)
+        restart.setOnClickListener {
+            restart()
         }
-        img.isClickable = false
+
     }
 
-    private fun isWinner(a: Int) {
-        var count = 0
+    override fun onClick(p0: View?) {
+        val img = findViewById<ImageButton>(p0!!.id)
+        val t = img.tag.toString().toInt()
+        val col: Int = t / 3
+        val row: Int = t % 3
+        if (matrix[col][row] == -1) {
+            if (active) {
+                img.setImageResource(R.drawable.x_sign)
+                active = false
+                matrix[col][row] = 1
+                active_player.text = "Player O"
+                isWinner(1)
+            } else {
+                img.setImageResource(R.drawable.o_sign)
+                active = true
+                matrix[col][row] = 0
+                active_player.text = "Player X"
+                isWinner(0)
+            }
+        }
 
+
+    }
+
+    var count = 0
+    private fun isWinner(a: Int) {
+        // by horizontal side e.g: left to right
+        horizontalCheck(a)
+        count = 0
+
+        // by vertical side e.g: top to bottom
+        verticalCheck(a)
+        count = 0
+
+        // x 0 0
+        // 0 x 0
+        // 0 0 x
+        fromLeftTopToRightBottomCheck(a)
+        count = 0
+
+        // 0 0 x
+        // 0 x 0
+        // x 0 0
+        fromRightTopToBottomCheck(a)
+    }
+
+    private fun horizontalCheck(a: Int) {
         for (i in 0..2) {
             for (j in 0..2) {
                 if (matrix[i][j] == a) {
                     count++
                 }
             }
+            if (count == 3) {
+                winner.text = "Winner is $a"
+                finishGame()
+                return
+            }
+            count = 0
         }
-        if (count == 3) {
-            winner.text = " ${a.toString()} is winner"
-            return
-        }
-        count = 0
+    }
+
+    private fun verticalCheck(a: Int) {
         for (i in 0..2) {
             for (j in 0..2) {
                 if (matrix[j][i] == a) {
                     count++
                 }
             }
+            if (count == 3) {
+                winner.text = "Winner is $a"
+                finishGame()
+                return
+            }
+            count = 0
         }
-        if (count == 3) {
-            winner.text = " ${a.toString()} is winner"
-            return
-        }
-        count = 0
+    }
+
+    private fun fromLeftTopToRightBottomCheck(a: Int) {
         for (i in 0..2) {
             for (j in 0..2) {
                 if (i == j) {
-                    count++
+                    if (matrix[j][i] == a) {
+                        count++
+                    }
                 }
             }
         }
         if (count == 3) {
-            winner.text = "${a.toString()} is winner "
+            winner.text = "Winner is $a"
+            finishGame()
             return
         }
-        count = 0
+    }
+
+    private fun fromRightTopToBottomCheck(a: Int) {
         for (i in 0..2) {
             for (j in 0..2) {
                 if (i + j == 2) {
-                    count++
+                    if (matrix[j][i] == a) {
+                        count++
+                    }
                 }
             }
         }
         if (count == 3) {
-            winner.text = "${a.toString()} is winner "
+            winner.text = "Winner is $a"
+            finishGame()
             return
         }
+    }
+
+    private fun finishGame() {
+        img0.isEnabled = false
+        img1.isEnabled = false
+        img2.isEnabled = false
+        img3.isEnabled = false
+        img4.isEnabled = false
+        img5.isEnabled = false
+        img6.isEnabled = false
+        img7.isEnabled = false
+        img8.isEnabled = false
+        restart.visibility = View.VISIBLE
+    }
+
+    private fun restart() {
+        matrix = Array(3) { IntArray(3) { -1 } }
+        active = true
+        active_player.text = "Player X"
+
+        restart.visibility = View.INVISIBLE
+
+        winner.text = ""
+
+        img0.isEnabled = true
+        img1.isEnabled = true
+        img2.isEnabled = true
+        img3.isEnabled = true
+        img4.isEnabled = true
+        img5.isEnabled = true
+        img6.isEnabled = true
+        img7.isEnabled = true
+        img8.isEnabled = true
+
+        img0.setImageDrawable(null)
+        img1.setImageDrawable(null)
+        img2.setImageDrawable(null)
+        img3.setImageDrawable(null)
+        img4.setImageDrawable(null)
+        img5.setImageDrawable(null)
+        img6.setImageDrawable(null)
+        img7.setImageDrawable(null)
+        img8.setImageDrawable(null)
     }
 }
